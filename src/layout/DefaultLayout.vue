7487a8aedaa4d075e3d64a3f68248a1fbc6d6308
unknown <bacemkhlifi001@gmail.com>
2026-04-25 12:17:51 +0100

    fix footer test

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -5,6 +5,9 @@
     <main class="flex-1 pt-24 md:pt-28">
       <router-view />
     </main>
+    <div style="background:red;color:white;padding:20px;text-align:center">
+  DEPLOY TEST FOOTER AREA
+</div>
 
     <Footer />
   </div>
